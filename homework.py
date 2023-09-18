104e4458535a77d3b288ff48695d14f840d966fe
Shokiroff <shodmon.1991@mail.ru>
2023-09-18 16:59:39 +0500

    Homeworkni bajarishni o'zim boshlab berdim

---
+++ homework.py
@@ -0,0 +1,7 @@
+def function_Shokiroff(age):
+    if age <5:
+        print("Siz uchun alohida bilet sotilmaydi")
+    elif age > 5 and age <10:
+        print("Siz uchun alohida bolalar bileti sotiladi")
+    else:
+        print("Ota-onayiz endi sizni olib yurmaydi chhunki bilet pullik")
