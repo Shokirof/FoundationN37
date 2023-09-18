c37c8ba82487d35d1a1f4be1d850f243363bde27
AcciGen <denrov13@gmail.com>
2023-09-18 18:59:59 +0500

    Accepted

--- homework.py
+++ homework.py
@@ -5,3 +5,44 @@ def function_Shokiroff(age):
         print("Siz uchun alohida bolalar bileti sotiladi")
     else:
         print("Ota-onayiz endi sizni olib yurmaydi chhunki bilet pullik")
+
+
+def MedianOfTwoLists_function_Asrorov_1(nums1, nums2):
+    nums1.extend(nums2)
+    if len(nums1) % 2 == 0:
+        return (sorted(nums1)[len(nums1) // 2] + sorted(nums1)[len(nums1) // 2 - 1]) / 2 
+    return sorted(nums1)[len(nums1)//2]
+
+
+def MukammalSon_function_Asrorov_2(num):
+    lst = []
+    for i in range(1, num//2 + 1):
+        if num % i == 0:
+            lst.append(i)
+
+    if sum(lst) == num:
+        return True
+    else:
+        return False
+
+
+def Polindrom_function_Asrorov_3(num):
+    if num == num[::-1]:
+        return True
+    return False
+
+
+def LastWordLength_function_Asrorov_4(text):
+        return len(text.split()[-1])
+
+
+def Factorial_function_Asrorov_5(nums):
+    lst = []
+    for i in range(len(nums)):
+        cnt = 1
+        x = 1
+        while x != nums[i]+1:
+            cnt *= x
+            x += 1
+        lst.append(cnt)
+    return lst
