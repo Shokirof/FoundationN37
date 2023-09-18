e2968a23e4593a375c0554ccade942e2d03ec505
Bekzod <bekzodtoxtamuratov@gmail.com>
2023-09-18 19:35:17 +0300

    da

--- homework.py
+++ homework.py
@@ -105,117 +105,45 @@ print(ekub_Toxtamuratov(24,36))
 ###########################################
 
 
-# 1-masala
-def list_sum_Toxtamuratov(num_List):
-    if len(num_List) == 1:
-     return num_List[0]
-    else:
-     return num_List[0] + list_sum_Toxtamuratov(num_List[1:])
-print(list_sum_Toxtamuratov([6,8,9,12,15,16]))
-
-# Faktorial Rekursiya
-# 2-masala
-def Toxtamuratov_Faktorial(n):
- if n <= 1:
-  return 1
- else:
-   return n * (Toxtamuratov_Faktorial(n - 1))
-
-#print(Toxtamuratov_Faktorial(5))
-
-# rejursiya orqali sonning raqamlar yogindisini hisoblaydigan dastur tu
-# 3-masala
-def Toxtamuratovdef_sumDigits(n):
-  if n == 0:
-   return 0
-  else:
-   return n % 10 + Toxtamuratovdef_sumDigits(int(n / 10))
-#print(Toxtamuratovdef_sumDigits(148))
-
-
-# a ning b ning darajasini hisoblaydigan dastur
-# 4-masala
-def powerToxtamuratov(a,b):
-    if b==0:
-     return 1
-    elif a==0:
-     return 0
-    elif b==1:
-     return a
-    else:
-     return a*powerToxtamuratov(a,b-1)
-#print(powerToxtamuratov(2,7))
 
 
 
-# 5-masala
-def ekub_Toxtamuratov(a,b):
-    low = min(a, b)
-    high = max(a, b)
-    if low == 0:
-     return high
-    elif low == 1:
-     return 1
-    else:
-     return ekub_Toxtamuratov(low, high%low)
-print(ekub_Toxtamuratov(24,36))
-
-###########################################
-
-
-# 1-masala
-def list_sum_Toxtamuratov(num_List):
-    if len(num_List) == 1:
-     return num_List[0]
-    else:
-     return num_List[0] + list_sum_Toxtamuratov(num_List[1:])
-print(list_sum_Toxtamuratov([6,8,9,12,15,16]))
-
-# Faktorial Rekursiya
-# 2-masala
-def Toxtamuratov_Faktorial(n):
- if n <= 1:
-  return 1
- else:
-   return n * (Toxtamuratov_Faktorial(n - 1))
-
-#print(Toxtamuratov_Faktorial(5))
-
-# rejursiya orqali sonning raqamlar yogindisini hisoblaydigan dastur tu
-# 3-masala
-def Toxtamuratovdef_sumDigits(n):
-  if n == 0:
-   return 0
-  else:
-   return n % 10 + Toxtamuratovdef_sumDigits(int(n / 10))
-#print(Toxtamuratovdef_sumDigits(148))
-
-
-# a ning b ning darajasini hisoblaydigan dastur
-# 4-masala
-def powerToxtamuratov(a,b):
-    if b==0:
-     return 1
-    elif a==0:
-     return 0
-    elif b==1:
-     return a
-    else:
-     return a*powerToxtamuratov(a,b-1)
-#print(powerToxtamuratov(2,7))
 
 
 
-# 5-masala
-def ekub_Toxtamuratov(a,b):
-    low = min(a, b)
-    high = max(a, b)
-    if low == 0:
-     return high
-    elif low == 1:
-     return 1
-    else:
-     return ekub_Toxtamuratov(low, high%low)
-print(ekub_Toxtamuratov(24,36))
 
-###########################################
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
