9380bab375c62deababa03b4a3c895d0fd6213f3
Pathogen <sabirjanovilyosbek@gmail.com>
2023-09-19 16:10:04 +0500

    robot? masala yechim filega qo'shildi

--- masalaYechimSobirjonov.py
+++ masalaYechimSobirjonov.py
@@ -1,4 +1,4 @@
-#IELTS
+"""#IELTS
 s, w, l, r = map(float, input().split())
 
 average_score = (s + w + l + r) / 4
@@ -13,3 +13,36 @@ else:
     ielts_score = int(average_score) + 1
 
 print(ielts_score)
+"""
+
+'''
+#Robot
+N = int(input())
+moves = list(map(int, input().split()))
+
+x, y = 0, 0
+direction = "N"
+
+directions = {"N": (0, 1), "E": (1, 0), "S": (0, -1), "W": (-1, 0)}
+
+def turn_clockwise(curr_direction):
+    if curr_direction == "N":
+        return "E"
+    elif curr_direction == "E":
+        return "S"
+    elif curr_direction == "S":
+        return "W"
+    else:
+        return "N"
+
+for move in moves:
+    dx, dy = directions[direction]
+    
+    x += move * dx
+    y += move * dy
+    
+    direction = turn_clockwise(direction)
+
+print(x, y)
+
+'''
